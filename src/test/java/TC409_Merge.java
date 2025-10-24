786b870bb830039e83f1847df248ae5575d61cd1
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-10-25 01:09:50 +0200

    Update TC409_Merge.java

--- src/test/java/TC409_Merge.java
+++ src/test/java/TC409_Merge.java
@@ -23,7 +23,7 @@ public class TC409_Merge extends BaseGUITest {
 
     @Test(priority = 1, description = "Verify user can login successfully")
     public void login() {
-        LoginHelper.login(loginPage, menuPage, LOGGER, username, password);
+        LoginHelper.login(loginPage, menuPage, LOGGER);
     }
 
     @Test(priority = 2, description = "Find two patients and record their IDs")
