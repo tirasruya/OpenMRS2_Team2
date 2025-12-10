1fa47c71be6e115c627777377e7d671e0668b1eb
tirasruya <tirasruya@gmail.com>
2025-12-10 21:58:30 +0300

    BaseDriver updated

--- src/test/java/utility/BaseDriver.java
+++ src/test/java/utility/BaseDriver.java
@@ -2,15 +2,21 @@ package utility;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
 public class BaseDriver {
 
     public static WebDriver driver(String url) {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless=new");
+        chromeOptions.addArguments("--window-size=1920,1080");
+
         WebDriver driver = new ChromeDriver();
         driver.get(url);
-        driver.manage().window().maximize();
+        //driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         return driver;
